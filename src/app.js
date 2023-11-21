a5654d177295f20edccb9bdae65ef530ecac35c7
Kevin Dutzy <kddutz15@gmail.com>
2023-11-21 11:19:35 -0700

    completed part 2 and 3

--- src/app.js
+++ src/app.js
@@ -3,13 +3,34 @@ const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get("/restaurants", async (req, res) => {
     const allRestaurants = await Restaurant.findAll()
     res.json(allRestaurants)
 })
 
-app.get("/restaurant/:id", async (req, res) => {
-    
+app.get("/restaurants/:id", async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    res.json(restaurant)
+})
+
+app.post("/restaurants", async (req, res) => {
+    const restaurant = await Restaurant.create(req.body)
+    res.json(restaurant)
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    restaurant.update(req.body)
+    res.json(restaurant)
+})
+
+app.delete("/restaurants/:id", async (req, res) => {
+    const restaurant = await Restaurant.findByPk(req.params.id)
+    restaurant.destroy()
+    res.sendStatus(res.status)
 })
 
 
